5ae897bb25850c30ca673e7e3bc6097bfa525536
Ariel Corte <cortearielformal@gmail.com>
2022-05-27 12:20:29 -0300

    added OpenGraph

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,6 +7,7 @@ import Products from '../features/products/Products'
 import Cart from '../features/cart/Cart'
 import { Helmet } from 'react-helmet'
 import favicon from '../common/assets/icons/favicon.ico'
+import head_image from '../common/assets/images/Products-Home-page.png'
 
 const App = () => {
   return (
@@ -14,6 +15,17 @@ const App = () => {
       <BrowserRouter>
         <Helmet>
           <link rel='shortcut icon' href={favicon} type='image/x-icon' />
+          <meta property='og:title' content='amiiboStore' />
+          <meta
+            property='og:description'
+            content='This project is an e-commerce mockup. You can add products to cart, remove them and choose their quantity.'
+          />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:url'
+            content='https://arielcorte-amiibo.vercel.app'
+          />
+          <meta property='og:image' content={head_image} />
         </Helmet>
         <Routes>
           <Route path='/' element={<Layout />}>
